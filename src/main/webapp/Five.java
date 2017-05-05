733013a4371c4ca4d02a7ff9ba521a1aa6dd7624
aithentgit <aithent.kumawat@gmail.com>
2017-05-05 17:35:56 +0530

    updated by rama
    adding the line

--- src/main/webapp/Five.java
+++ src/main/webapp/Five.java
@@ -7,6 +7,7 @@ public static void main(String args[]){
 }
 
 public void add(){
+   System.out.println("hi Praveen");
    System.out.println("hi this is rama");
    System.out.println(Hi dosto, rama);
    System.out.println(how are you);
